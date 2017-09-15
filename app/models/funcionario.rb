8098d0225f7753254c0fd189fcdcbbfed9095bf7
cabanga <manuelcabanga@hotmail.com>
2017-09-15 09:02:38 +0100

    Adiciona Validações ao adicionar funcionário

--- app/models/funcionario.rb
+++ app/models/funcionario.rb
@@ -24,4 +24,30 @@ class Funcionario < ApplicationRecord
 
   end
 
+  validate :validar_email, :validar_telemovel
+
+  private
+
+  validates :nome,
+     presence: {message: 'não pode estar em branco'},
+     format: {with: NOME_REGEX, message: "caracteres inválidos. Ex: Hermenegildo ou Beatriz Madalena"},
+     length: {in: 6..50, message: "nome muito curto, Ex: José Pedro ou Adalberto Francisco"}
+
+    validates :telemovel, uniqueness: true,
+              presence: {message: "não pode estar em branco!"},
+              :numericality => {:only_integer => true, message: "numero de telefone inválido"},
+              length: {in: 9..9, message: "número de telefone inválido. Ex: 923456699"}
+
+  def validar_email
+    if Usuario.find_by(email: self.email)
+      errors.add(:email, 'endereço de email indisponível')
+    end
+  end
+
+  def validar_telemovel
+    if Usuario.find_by(telemovel: self.telemovel)
+      errors.add(:telemovel, 'Número de telemóvel indisponível')
+    end
+  end
+
 end
