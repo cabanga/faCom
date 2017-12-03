f3ebc18d757719fa39b4f020d7b9e569835dd695
cabanga <manuelcabanga@hotmail.com>
2017-12-03 18:53:56 +0100

    Configuração do DrpBox V2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
+require 'dropbox'
+
 CarrierWave.configure do |config|
-  config.dropbox_app_key = "01pk7m6ugz55uhv"
-  config.dropbox_app_secret = "abvmdkkdmo4cb8i"
-  config.dropbox_access_token = "swv3ix53qkyn0szj"
-  config.dropbox_access_token_secret = "b7075awytodqy6n"
-  config.dropbox_user_id = "659914788"
-  config.dropbox_access_type = "app_folder"
+  dbx = Dropbox::Client.new(ENV['DROPBOX_ACCESS_TOKEN_FACOM'])
+
+  puts "****************************************"
+  p dbx
 
   if Rails.env.production?
     config.storage :dropbox
