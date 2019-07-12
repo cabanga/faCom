19531edb1b2ad4d980abd881199e90a8498e1737
cabanga <manuelcabanga@hotmail.com>
2019-07-12 23:28:25 +0100

    update email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,15 +16,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'fa-com.herokuapp.com' }
 
 # ==============================================================================
+# ==============================================================================
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            'facomtechys@gmail.com',
-    password:             '21cabanga21',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  user_name:            'facomtechys@gmail.com',
+  password:             '21cabanga21',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
 # ==============================================================================
 
